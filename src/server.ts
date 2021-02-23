2e51f0ad2fe747e04b73a865e2e79334cdada431
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-23 08:20:24 -0300

    🚀 added comma

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.json({ rockeaseat: 'Hello World NLW 4' }))
+app.get("/", (req, res) => res.json({ rockeaseat: "Hello World NLW 4" }));
 
 // eslint-disable-next-line no-console
-app.listen(3000, () => console.log('Server is running!!'))
+app.listen(3000, () => console.log("Server is running!!"));
