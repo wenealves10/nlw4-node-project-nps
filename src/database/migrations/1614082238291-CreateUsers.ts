6b5d0e42003a7a4ddebcc3f0fa27eeb63e3e908b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-23 11:39:30 -0300

    🛠 added migrations

---
+++ src/database/migrations/1614082238291-CreateUsers.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1614082238291 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    await _queryRunner.dropTable("users");
+  }
+}
