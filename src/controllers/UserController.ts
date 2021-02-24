9384b850e21e8965c34f89df5cfdf8352777dc6d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-24 19:35:58 -0300

    🛠 modify userController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
   async create(request: Request, response: Response) {
     try {
       const { name, email } = request.body;
 
-      const userRepository = getRepository(User);
+      const userRepository = getCustomRepository(UsersRepository);
 
       const userAlreadyExists = await userRepository.findOne({ email });
 
@@ -22,7 +22,7 @@ class UserController {
 
       await userRepository.save(user);
 
-      return response.json({ user });
+      return response.status(201).json({ user });
     } catch (error) {
       return response.status(400).json({ error });
     }
