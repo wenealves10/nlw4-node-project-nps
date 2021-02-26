11e5e5451160d4382a5868165e288109a8386eb9
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-26 14:23:27 -0300

    🚩🚩 added new routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
@@ -7,6 +9,8 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveyController();
 const sendMailController = new SendMailController();
+const answersController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -14,5 +18,9 @@ router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answersController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
